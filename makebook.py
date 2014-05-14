836b7212511ad08c2b8da24d731be7cb1a46cfd3
mie00 <melawadi2005@yahoo.com>
2014-05-14 07:17:30 +0200

    initial commit

---
+++ makebook.py
@@ -0,0 +1,23 @@
+import pyodbc
+from os import makedirs,path,listdir
+def generate(number):
+	DBfile = 'data\\%d.mdb'%(number)
+	if not path.exists('op\\%d'%(number)):
+		makedirs('op\\%d'%(number))
+	conn = pyodbc.connect('DRIVER={Microsoft Access Driver (*.mdb)};DBQ='+DBfile)
+	cursor = conn.cursor()
+	 
+	SQL = 'SELECT * FROM book ORDER BY id'
+	for row in cursor.execute(SQL): # cursors are iterable
+	    with open('op\\%d\\%d.html'%(number,row.id),'wb') as f:
+
+	    	f.write('<!DOCTYPE html><html><meta charset="windows-1256"><body dir="rtl">\
+	    		<p><a href="%d.html">&lt;</a><a href="../../index.html">&uarr;</a><a href="%d.html">&gt;</a></p>\
+	    		<p>%s</p></body></html>'%(row.id-1,row.id+1,row.nass.replace('\r','<br />')))
+	cursor.close()
+	conn.close()
+
+for i in listdir('data'):
+	generate(int(i[:-4]))
+
+import updateindex
---
+++ updateindex.py
@@ -0,0 +1,30 @@
+import pyodbc
+from os import listdir
+DBfile = 'main.mdb'
+conn = pyodbc.connect('DRIVER={Microsoft Access Driver (*.mdb)};DBQ='+DBfile)
+cursor = conn.cursor()
+
+def get_book_name(id):
+	SQL = 'SELECT bk FROM 0bok WHERE bkid=%i'%(id)
+	for row in cursor.execute(SQL): # cursors are iterable
+		return row.bk
+def format_book(id1):
+	id=int(id1)
+	return '<a href="op\\%i\\1.html">%s</a>'%(id,get_book_name(id))
+
+
+op='<!doctype html>\
+<html lang="en">\
+<head>\
+	<meta charset="windows-1256">\
+	<title>Document</title>\
+</head>\
+<body dir="rtl">\
+%s\
+</body>\
+</html>'%('<br />'.join(map(format_book,listdir('op'))))
+with open('index.html','wb') as f:
+	f.write(op)
+cursor.close()
+conn.close()
+
